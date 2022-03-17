96ef4a707077444481e627a7cefee5b6dbcf3eb5
Elena Bakova <lenabakova72@gmail.com>
2022-03-17 14:51:17 +0300

    Changed

--- Homework3/lambda-interpreter/Program.fs
+++ Homework3/lambda-interpreter/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿
+type Term =
+    |Variable of string
+    |Abstraction of string * Term
+    |Application of Term * Term
+
+let betaReduction term = 
+    let rec reduction term=
+        match term with
+        | Variable(var) -> Variable(var)
+        | Abstraction(variable, term) -> Abstraction(variable, reduction(term))
+        | Application(term1, term2) ->
+            match term1 with
+            | Abstraction(variable, term3) -> term3// подставить variable into term2
+            | _ -> Application(reduction(term1), reduction(term2))
+    reduction term
