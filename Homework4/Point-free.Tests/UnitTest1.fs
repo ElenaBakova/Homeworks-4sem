e4fadf5bfc3e225087c07247536a85a6105006ea
Elena Bakova <lenabakova72@gmail.com>
2022-03-23 19:59:51 +0300

    Changed tests

--- Homework4/Point-free.Tests/UnitTest1.fs
+++ Homework4/Point-free.Tests/UnitTest1.fs
@@ -2,9 +2,18 @@ module Point_free.Tests
 
 open NUnit.Framework
 open Functions
+open FsUnit
 open FsCheck
 
 [<Test>]
+let ``Check whether myFunction is correct`` () =
+    let answerList = [2; 4; 8; 16; 32]
+    let list = [1; 2; 4; 8; 16]
+    myFunction 2 list |> should equal answerList
+
+[<Test>]
 let ``Point-free and myFunction equality check`` () =
-    let check param list = myFunction param list = myFunction5 param list
-    Check.QuickThrowOnFailure check
+    let myFunction6 = List.map << (*)
+    let intGenerator = Arb.generate<int>
+    let list = intGenerator |> Gen.sample 1000 100
+    myFunction 5 list |> should equal (myFunction6 5 list)
--- Homework4/Point-free/Program.fs
+++ Homework4/Point-free/Program.fs
@@ -8,4 +8,6 @@ let myFunction3 x = List.map ((*) x)
 
 let myFunction4 x = List.map << (*) << x
 
-let myFunction5  = List.map << (*)
+let myFunction5 = List.map << (*)
+
+printfn "%A" (myFunction5 2 [1; 2; 4; 8; 5; 8])
