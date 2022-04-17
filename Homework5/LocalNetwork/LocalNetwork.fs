a7596e1220c4a4e30c24bd29568d1c0aab6b84ad
Elena Bakova <lenabakova72@gmail.com>
2022-04-17 21:42:41 +0300

    Init

---
+++ Homework5/LocalNetwork/LocalNetwork.fs
@@ -0,0 +1,16 @@
+﻿module LocalNetwork
+
+type OS =
+    | Windows
+    | MacOS
+    | Linux
+
+type Infected =
+    | Clear
+    | Now
+    | NotNow
+
+type Computer (operatingSystem: OS, computerID: string, isInfected: Infected) =
+    member this.os = operatingSystem
+    member this.id = computerID
+    member val IsInfected = isInfected
