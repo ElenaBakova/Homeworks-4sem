a09b33375a38e2336d9c7c699e721b161e84aa93
Elena Bakova <lenabakova72@gmail.com>
2022-04-17 20:58:47 +0300

    Init

---
+++ Homework7/Workflow.Tests/Tests.fs
@@ -0,0 +1,42 @@
+﻿module Workflow.Tests
+
+open NUnit.Framework
+open Builders
+
+[<TestCase(2, 0.05)>]
+[<TestCase(3, 0.048)>]
+[<TestCase(4, 0.0476)>]
+let CounterBuilderTest (digits: int, expected) =
+    let rounding x = new CounterBuilder(x)
+    let result = rounding digits {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    Assert.AreEqual(expected, result)
+
+[<TestCase("1", "2", 3)>]
+[<TestCase("15", "2", 17)>]
+[<TestCase("-1", "5", 4)>]
+let StringCounterBuilderTest (a: string, b: string, expected) =
+    let calculate = new StringCounterBuilder()
+    let result = calculate {
+        let! x = a
+        let! y = b
+        let z = x + y
+        return z
+    }
+    Assert.AreEqual(expected |> Some, result)
+    
+[<TestCase("1", "X")>]
+[<TestCase("1Y", "2")>]
+[<TestCase("-1", "5.")>]
+let StringCounterBuilderTest2 (a: string, b: string) =
+    let calculate = new StringCounterBuilder()
+    let result = calculate {
+        let! x = a
+        let! y = b
+        let z = x + y
+        return z
+    }
+    Assert.AreEqual(None, result)
---
+++ Homework7/Workflow/Builders.fs
@@ -0,0 +1,17 @@
+﻿module Builders
+
+open System
+
+/// Workflow for couting with given accuracy 
+type CounterBuilder (digits: int) =
+    member this.Bind(x, func) = func x
+    member this.Return(x: float) = Math.Round(x, digits)
+
+/// Workflow for couting with numbers given as a string
+type StringCounterBuilder () =
+    member this.Bind(x: string, func) = 
+        try 
+            x |> int |> func
+        with :? FormatException
+            -> None
+    member this.Return(x) = x |> Some
