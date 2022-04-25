881b9316604300c181c664db162781b9b1dbb7f9
Elena Bakova <lenabakova72@gmail.com>
2022-04-25 13:22:52 +0300

    Fixed tests

--- Test/Task2/BinTree.Tests/TreeTests.fs
+++ Test/Task2/BinTree.Tests/TreeTests.fs
@@ -6,17 +6,17 @@ open FsUnit
 [<Test>]
 let ``Get only even nodes`` () =
     let tree = Node(3, Node(6, Node(8, Tree.Empty, Tree.Empty), Tree.Empty), Node(9, Tree.Empty, Tree.Empty))
-    let expected = [6; 8]
+    let expected = [8; 6]
     getList tree (fun x -> x % 2 = 0) |> should equal expected
 
 [<Test>]
 let ``Get nodes greater than 5`` () =
     let tree = Node(3, Node(6, Node(8, Tree.Empty, Tree.Empty), Tree.Empty), Node(9, Node(10, Node(18, Tree.Empty, Tree.Empty), Tree.Empty), Tree.Empty))
-    let expected = [6; 8; 9; 10; 18]
+    let expected = [18; 10; 9; 8; 6]
     getList tree (fun x -> x > 5) |> should equal expected
 
 [<Test>]
 let ``Get nodes containing letter a`` () =
     let tree = Node("aba", Node("caba", Node("db", Tree.Empty, Tree.Empty), Tree.Empty), Node("bc", Tree.Empty, Tree.Empty))
-    let expected = ["aba"; "caba"]
+    let expected = ["caba"; "aba"]
     getList tree (fun x -> x.Contains('a')) |> should equal expected
