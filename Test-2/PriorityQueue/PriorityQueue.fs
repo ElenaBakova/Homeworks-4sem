e854965644a1ce5e331ac9ef153cf0c36281b89e
Elena Bakova <lenabakova72@gmail.com>
2022-05-05 14:50:14 +0300

    init

---
+++ Test-2/PriorityQueue/PriorityQueue.fs
@@ -0,0 +1,8 @@
+﻿module PriorityQueue
+
+type PriorityQueue<'a> (n)= 
+    let queue: 'a array = Array.zeroCreate(n)
+    let size() = queue.Length
+    
+    member _.IsEmpty with get() = size() = 0
+
