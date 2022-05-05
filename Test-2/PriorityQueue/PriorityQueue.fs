416d5a1e41738608ec37eeb65ee948f4d3e79b60
Elena Bakova <lenabakova72@gmail.com>
2022-05-05 15:19:20 +0300

    Amended

--- Test-2/PriorityQueue/PriorityQueue.fs
+++ Test-2/PriorityQueue/PriorityQueue.fs
@@ -1,8 +1,28 @@
 ﻿module PriorityQueue
 
-type PriorityQueue<'a> (n)= 
-    let queue: 'a array = Array.zeroCreate(n)
-    let size() = queue.Length
-    
-    member _.IsEmpty with get() = size() = 0
+open System
 
+type PriorityQueue<'a when 'a :> IComparable> (n)= 
+    let mutable queue: ('a * int) list = []
+    let size() = queue.Length
+
+    let rec enqueueElement (value: 'a) (priority: int) list =
+        match list with
+        | [] -> [(value, priority)]
+        | ((head: (#IComparable * int)))::tail -> 
+            if head.Item2 <= priority then
+                head :: [(value, priority)] @ tail
+            else 
+                head :: (enqueueElement value priority tail)
+
+    let enqueue (value: 'a) priority = 
+        queue <- (enqueueElement value priority queue)
+
+    //let dequeueElement list =
+
+    let dequeue() = 
+        if size() = 0 then
+            raise (InvalidOperationException("Can't dequeue from empty queue"))
+        //dequeueElement queue
+
+    member _.IsEmpty with get() = size() = 0
