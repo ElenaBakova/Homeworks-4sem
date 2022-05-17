b9e8923c420cefbea97d99374205ed774f257c83
Elena Bakova <lenabakova72@gmail.com>
2022-05-18 00:06:08 +0300

    Changed tests

--- Homework7/Mini-Crawler.Tests/UnitTest1.fs
+++ Homework7/Mini-Crawler.Tests/UnitTest1.fs
@@ -10,12 +10,12 @@ let Test1 () =
     let url = "https://se.math.spbu.ru/"
 
     let expected =
-        [ ("https://oops.math.spbu.ru/SE/alumni", 49175)
+        [ ("https://oops.math.spbu.ru/SE/alumni", Some 49175)
           ("https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2,_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B9_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87",
-           86294)
+           Some 86294)
           ("https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2,_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B9_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87",
-           86294)
-          ("https://oops.math.spbu.ru/SE/alumni", 49175) ]
+           Some 86294)
+          ("https://oops.math.spbu.ru/SE/alumni", Some 49175) ]
         |> Seq.ofList
 
     let result = getInfo url |> Async.RunSynchronously
