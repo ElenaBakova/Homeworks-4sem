f5fc179595db63a0ad69b866fb9dbfa7954e6751
Elena Bakova <lenabakova72@gmail.com>
2022-05-18 00:04:59 +0300

    Fixed

--- Homework7/Mini-Crawler/Crawler.fs
+++ Homework7/Mini-Crawler/Crawler.fs
@@ -17,31 +17,34 @@ let downloadPage (url: string) (client: HttpClient) =
     client.GetStringAsync(url)
     |> Async.AwaitTask
     |> Async.Catch
-    |> Async.RunSynchronously
 
-let getSizes (url: string) (client: HttpClient) =
-    downloadPage url client
-    |> (function
-    | Choice1Of2 result -> result.Length
-    | Choice2Of2 (ex: exn) -> failwith ex.Message)
+let getSizes pages =
+    pages
+    |> Seq.map (fun page ->
+        match page with
+        | Choice1Of2 (x: string) -> Some x.Length
+        | Choice2Of2 (_: exn) -> None)
 
 // Returns information about size of pages placed in the given page
 let getInfo (url: string) =
     async {
         let client = new HttpClient()
 
-        let html =
-            downloadPage url client
-            |> (function
-            | Choice1Of2 result -> result
-            | Choice2Of2 (ex: exn) -> failwith ex.Message)
+        let! page = downloadPage url client
+        let html = 
+            match page with
+            | Choice1Of2 result -> Some result
+            | Choice2Of2 (_: exn) -> None
 
 
-        let links = fetchLinks html
+        let links = fetchLinks html.Value
 
         let sizes =
             links
-            |> Seq.map (fun link -> getSizes link client)
+            |> Seq.map (fun link -> downloadPage link client)
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> getSizes
 
         return Seq.zip links sizes
     }
